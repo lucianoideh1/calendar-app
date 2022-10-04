027ad5217321bb6080ce012250254d1eb70560b4
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-03 23:31:43 -0300

    added current date and replaced the hardcoded days and made them render with js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("welcome")
+const currentDate = document.querySelector(".current-date"),
+daysTag = document.querySelector(".days")
+
+
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const renderCalender = () =>{
+    let lastDateOfMonth = new Date(currYear,currMonth,0).getDate();
+    let liTag = "";
+    for(let i = 1; i <= lastDateOfMonth;i++){
+        liTag += `<li>${i}</li>`;
+    }
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`
+    daysTag.innerHTML = liTag;
+}
+
+renderCalender();
